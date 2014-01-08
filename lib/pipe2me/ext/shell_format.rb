radiospiel/pipe2me-client

commit fe0283cde4b4f6ee8d660c56442d7f45ddb05fba

pipe2me env command
The pipe2me env subcommand shows the settings for the tunnel 
configuration in the local directory.

lib/pipe2me/ext/shell_format.rb
@@ -14,8 +14,12 @@ module Pipe2me::ShellFormat
 
   alias :shell :dump
 
+  PREFIX = "PIPE2ME"
+
   def parse(data)
     arrays = {}
+
+    prefix_re = Regexp.compile(/^#{PREFIX.downcase}_/)
 
     data.lines.inject({}) do |hsh, line|
       key, value = line.split(/\s*=\s*/, 2)
@@ -28,7 +32,7 @@ module Pipe2me::ShellFormat
         key, value = $1, ary
       end
 
-      hsh.update key.downcase.to_sym => value
+      hsh.update key.downcase.gsub(prefix_re, "").to_sym => value
     end
   end
 
@@ -54,7 +58,7 @@ module Pipe2me::ShellFormat
       if obj.respond_to?(:attributes)
         format_entries(ary, obj.attributes, prefix)
       else
-        ary << "#{prefix}=#{Shellwords.escape(obj)}\n"
+        ary << "#{PREFIX}_#{prefix}=#{Shellwords.escape(obj)}\n"
       end
     end
   end

doc/pipe2me.1
@@ -10,6 +10,9 @@
 \fBpipe2me\fR [ options ] setup \.\.\.
 .
 .br
+\fBpipe2me\fR [ options ] env
+.
+.br
 .
 .SH "DESCRIPTION"
 \fBpipe2me\fR is a client for the pipe2me server package, which hels tunnels TCP connections through routers\.
@@ -70,7 +73,7 @@ local ports, one per protocol
 .
 .IP "" 0
 .
-.SH "EXAMPLES"
+.SS "EXAMPLES"
 Setup 3 tunnels under a specific FQDN:
 .
 .IP "" 4
@@ -82,6 +85,28 @@ $ pipe2me setup \-\-protocols=https,http,imap
 .fi
 .
 .IP "" 0
+.
+.SH "SHOW TUNNEL SETTINGS"
+\fBpipe2me\fR env
+.
+.br
+.
+.P
+shows the tunnel configuration in the current directory\.
+.
+.SS "EXAMPLES"
+.
+.nf
+
+$ pipe2me env
+\./bin/pipe2me env
+PIPE2ME_SERVER=http://pipe2\.dev
+PIPE2ME_TOKEN=69o1ykbfzggz9lcd6qjzkggq7
+PIPE2ME_FQDN=handsome\-snowwhite\-hamster\.pipe2\.dev
+PIPE2ME_URLS_0=https://handsome\-snowwhite\-hamster\.pipe2\.dev:10049
+PIPE2ME_TUNNEL=ssh://eno@pipe2\.dev:4444
+.
+.fi
 .
 .SH "BUGS"
 The installed version of \fBpipe2me\fR is written in Ruby\. A more portable and version of this program \- e\.g\. in bash \- would be welcome\.

doc/pipe2me.1.html
@@ -59,7 +59,7 @@
     <a href="#FILES">FILES</a>
     <a href="#OPTIONS">OPTIONS</a>
     <a href="#TUNNEL-SETUP">TUNNEL SETUP</a>
-    <a href="#EXAMPLES">EXAMPLES</a>
+    <a href="#SHOW-TUNNEL-SETTINGS">SHOW TUNNEL SETTINGS</a>
     <a href="#BUGS">BUGS</a>
     <a href="#COPYRIGHT">COPYRIGHT</a>
     <a href="#SEE-ALSO">SEE ALSO</a>
@@ -78,7 +78,8 @@
 
 <h2 id="SYNOPSIS">SYNOPSIS</h2>
 
-<p><code>pipe2me</code> [ options ] setup ...<br /></p>
+<p><code>pipe2me</code> [ options ] setup ...<br />
+<code>pipe2me</code> [ options ] env<br /></p>
 
 <h2 id="DESCRIPTION">DESCRIPTION</h2>
 
@@ -122,11 +123,28 @@ local ports, one per protocol</li>
 </ul>
 
 
-<h2 id="EXAMPLES">EXAMPLES</h2>
+<h3 id="EXAMPLES">EXAMPLES</h3>
 
 <p>Setup 3 tunnels under a specific FQDN:</p>
 
 <pre><code>$ pipe2me setup --protocols=https,http,imap
+</code></pre>
+
+<h2 id="SHOW-TUNNEL-SETTINGS">SHOW TUNNEL SETTINGS</h2>
+
+<p><code>pipe2me</code> env<br /></p>
+
+<p>shows the tunnel configuration in the current directory.</p>
+
+<h3 id="EXAMPLES">EXAMPLES</h3>
+
+<pre><code>$ pipe2me env
+./bin/pipe2me env
+PIPE2ME_SERVER=http://pipe2.dev
+PIPE2ME_TOKEN=69o1ykbfzggz9lcd6qjzkggq7
+PIPE2ME_FQDN=handsome-snowwhite-hamster.pipe2.dev
+PIPE2ME_URLS_0=https://handsome-snowwhite-hamster.pipe2.dev:10049
+PIPE2ME_TUNNEL=ssh://eno@pipe2.dev:4444
 </code></pre>
 
 <h2 id="BUGS">BUGS</h2>

doc/pipe2me.ronn
@@ -4,6 +4,7 @@ pipe2me(1) -- manage tunnels to a pipe2me server
 ## SYNOPSIS
 
 `pipe2me` [ options ] setup ...<br>
+`pipe2me` [ options ] env<br>
 
 ## DESCRIPTION
 
@@ -45,11 +46,27 @@ Options include:
   * `--local-ports`, `-l` ports:
     local ports, one per protocol
 
-## EXAMPLES
+### EXAMPLES
 
 Setup 3 tunnels under a specific FQDN:
 
     $ pipe2me setup --protocols=https,http,imap
+
+## SHOW TUNNEL SETTINGS
+
+`pipe2me` env<br>
+
+shows the tunnel configuration in the current directory.
+
+### EXAMPLES
+
+    $ pipe2me env
+    ./bin/pipe2me env
+    PIPE2ME_SERVER=http://pipe2.dev
+    PIPE2ME_TOKEN=69o1ykbfzggz9lcd6qjzkggq7
+    PIPE2ME_FQDN=handsome-snowwhite-hamster.pipe2.dev
+    PIPE2ME_URLS_0=https://handsome-snowwhite-hamster.pipe2.dev:10049
+    PIPE2ME_TUNNEL=ssh://eno@pipe2.dev:4444
 
 ## BUGS
 

lib/pipe2me/cli/commands.rb
@@ -11,4 +11,10 @@ module Pipe2me::CLI
     # update server_info[:fqdn]
     puts server_info[:fqdn]
   end
+
+  banner "show tunnel configuration"
+  def env(*args)
+    puts File.read("pipe2me.local.inc")
+    puts File.read("pipe2me.info.inc")
+  end
 end

test/env-test.sh
@@ -1,0 +1,34 @@
+#!/usr/bin/env roundup
+# `describe` the plan meaningfully.
+
+pipe2me=$(cd $(dirname $1)/../bin && pwd)/pipe2me
+
+before() {
+  mkdir scrub
+  cd scrub
+}
+
+after() {
+  cd ..
+  rm -rf scrub
+}
+
+describe "show pipe2me environment"
+it_start_a_tunnel() {
+  fqdn=$($pipe2me setup)
+  $pipe2me env > env
+  echo "== env is ============="
+  cat env
+  echo "== env done ============="
+
+  cat env | grep PIPE2ME_SERVER
+  cat env | grep PIPE2ME_TOKEN
+  cat env | grep PIPE2ME_FQDN
+  cat env | grep PIPE2ME_URLS_0
+  cat env | grep PIPE2ME_TUNNEL
+  ! (cat env | grep PIPE2ME_URLS_1)
+
+  # can we source the env?
+  eval $($pipe2me env)
+  echo $PIPE2ME_URLS_0 | grep http
+}

commit 991769063dd9d6f049991f077b46948c8e2abcd9

Fixes ShellFormat#parse
The fe0283cd introduced a regression which prevented arrays in 
ShellFormat files to be parsed correctly.

lib/pipe2me/ext/shell_format.rb
@@ -30,7 +30,7 @@ module Pipe2me::ShellFormat
       value.gsub!(/\s*$/, "")
       value = Integer(value) rescue value
 
-      if key =~ /^([^_]+)_(\d+)$/
+      if key =~ /^(.*)_(\d+)$/
         ary = arrays[$1] ||= []
         ary[$2.to_i] = value
         key, value = $1, ary