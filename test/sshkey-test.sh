37a31575e48a8ab717727c82251a820e8b14888b
radiospiel <eno@open-lab.org>
2014-01-13 14:42:42 +0100

    Various test improvements
    - run roundup test first
    - run against "debug" test setting when not specified.

--- test/sshkey-test.sh
+++ test/sshkey-test.sh
@@ -3,10 +3,15 @@
 
 . $(dirname $1)/testhelper.inc
 
+#
+# pipe2me setup creates ssh identity
+#
 describe "setup creates an ssh key"
-it_sets_up_tunnels() {
+it_sets_up_ssh_identity() {
   fqdn=$($pipe2me setup --server $pipe2me_server --auth $pipe2me_token)
   test -f pipe2me.id_rsa.pub
   test -f pipe2me.id_rsa
+
+  # identity must contain $fqdn
   cat pipe2me.id_rsa.pub | grep $fqdn
 }
