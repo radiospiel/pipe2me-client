1f627b73fc09c2235fc85f5b73bd42cf7e726764
radiospiel <eno@open-lab.org>
2014-01-18 09:20:03 +0100

    Adds --echo option to monit commands

--- lib/pipe2me/cli-monit.rb
+++ lib/pipe2me/cli-monit.rb
@@ -1,52 +1,57 @@
 class Pipe2me::CLI < Thor
-  MONITRC = "pipe2me.monitrc"
-
   desc "monit", "Create monitrc file and run monit"
-  option :port, :banner => "control port", :default => 5555
+  option :port, :default => 5555, :banner => "control port"
+  option :echo, :type => :boolean, :banner => "Also run echo servers"
   def monit(*args)
-    create_monitrc(options) unless File.exists?(MONITRC)
+    monitrc_file = create_monitrc
 
-    UI.warn "Running: monit -c #{MONITRC} #{args.join(" ")}"
-    Kernel.exec "monit", "-c", MONITRC, *args
+    UI.warn "Running: monit -c #{monitrc_file} #{args.join(" ")}"
+    Kernel.exec "monit", "-c", monitrc_file, *args
   end
 
   desc "monitrc", "Create monitrc file"
-  option :port, :banner => "control port", :default => 5555
+  option :port, :default => 5555, :banner => "control port"
+  option :echo, :type => :boolean, :banner => "Also run echo servers"
   def monitrc
-    create_monitrc(options)
-    Kernel.exec "monit", "-c", MONITRC, "-t"
+    monitrc_file = create_monitrc
+    Kernel.exec "monit", "-c", monitrc_file, "-t"
   end
 
   private
 
-  def create_monitrc(options)
+  def create_monitrc
+    path = options[:echo] ? "pipe2me.monitrc.echo" : "pipe2me.monitrc"
+
+    # The daemon binary
+    daemon_bin = `which daemon`.chomp
+    daemon = "#{daemon_bin} -D #{File.expand_path(Dir.getwd)}"
+
     port = options[:port]
+
     logfile = File.expand_path "pipe2me.monit.log"
     piddir = File.expand_path "pipe2me.monit.pids"
     FileUtils.mkdir_p piddir
 
-    File.atomic_write MONITRC do |io|
+    commands = Pipe2me::Tunnel.tunnel_commands
+    commands += Pipe2me::Tunnel.echo_commands if options[:echo]
+
+    File.open path, "w", 0600 do |io|
       require "erb"
 
       erb = ERB.new MONITRC_ERB
       io.write erb.result(self.send(:binding))
     end
 
-    FileUtils.chmod 0600, MONITRC
+    UI.success "Created #{path}"
 
-    UI.success "Created #{MONITRC}"
+    path
   end
 
 MONITRC_ERB =  %q{
-<%
-  daemon_bin = `which daemon`.chomp
-  daemon = "#{daemon_bin} -D #{File.expand_path(Dir.getwd)}"
-%>
-
 set daemon 10
 set httpd port <%= port %> and use address localhost allow localhost
 
-<% Pipe2me::Tunnel.tunnel_commands.each do |name, cmd| %>
+<% commands.each do |name, cmd| %>
 check process <%= name %> with pidfile <%= piddir %>/<%= name %>.pid
   <% daemon = "#{daemon} -N --name #{name} --pidfiles #{piddir} --output #{logfile}" %>
   start program = "<%= daemon %> -- <%= cmd %>" with timeout 60 seconds
