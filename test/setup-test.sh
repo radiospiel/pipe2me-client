d285dad3c0e6c3dba5791ee1317fe2bef71c13e8
radiospiel <eno@open-lab.org>
2014-01-23 16:11:41 +0100

    Fixes test:release to actually run against test.pipe2.me

--- test/setup-test.sh
+++ test/setup-test.sh
@@ -7,11 +7,13 @@ describe "tunnel setup"
 it_sets_up_tunnels() {
   fqdn=$($pipe2me setup --server $pipe2me_server --token $pipe2me_token --ports 8100,8101 --protocols http,https)
 
+  test "$fqdn" != ""
+
   # pipe2me setup --server $pipe2me_server returns the fqdn of the subdomain and nothing else
   test 1 -eq $(echo $fqdn | wc -l)
 
   # The subdomain is actually a subdomain.
-  echo $fqdn | grep \.pipe2\.dev
+  echo $fqdn | grep \.pipe2\.
 
   # Cannot setup a second tunnel in the same directory.
   ! $pipe2me setup --server $pipe2me_server --token $pipe2me_token
