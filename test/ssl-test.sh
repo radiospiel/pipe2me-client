8873ea8642030a98c9cd87f2cdc6db1aabb6f4ec
eno <eno@open-lab.org>
2014-05-01 14:07:45 +0200

    Verify ssl keys
    This test implements basic checks for SSL keys. More thorough tests
    about the SSL keys generated by the pipe2me server are to be implemented
    in the key generating software (miniCA)

--- test/ssl-test.sh
+++ test/ssl-test.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env roundup
-describe "openssl tests"
+describe "SSL tests"
+
+# This file contains tests related to SSL functionality.
+#
 
 . $(dirname $1)/testhelper.inc
 
 # setup creates and signs openssl credentials
-it_sets_up_openssl_certs() {
+it_sets_up_ssl_certs() {
+  # setup the pipe2me client. This must create a private key and a server
+  # certificate, amongst other things. The certificate must contain the
+  # correct name.
+
+
   fqdn=$($pipe2me setup --server $pipe2me_server --token $pipe2me_token)
+
+  # test for private key
   test -f pipe2me.openssl.priv
   cat pipe2me.openssl.priv | grep -E "BEGIN.*PRIVATE KEY"
 
+  # test for certificate
   test -f pipe2me.openssl.cert
   cat pipe2me.openssl.cert | grep "BEGIN CERTIFICATE"
 
-  # verify cert name
+  # verify name in certificate
   openssl x509 -in pipe2me.openssl.cert  -text | grep CN=$fqdn
-}
-
-it_cannot_sign_other_certs() {
-  later A certificate cannot be used to sign other certificates
-}
-
-it_cannot_sign_fake_certs() {
-  later A client cannot ask the server to sign certs with different names
 }
